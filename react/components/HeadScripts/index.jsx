493c0f228e6733ecc35b728d134bca6a09fc4d2a
Raul Oliveira <raulodl@gmail.com>
2024-07-03 17:09:01 -0300

    fix: chatbot removido temporariamente

--- react/components/HeadScripts/index.jsx
+++ react/components/HeadScripts/index.jsx
@@ -1,30 +1,30 @@
 import { useEffect } from "react";
 
 const HeadScripts = () => {
-  useEffect(() => {
-    const scriptElement = document.createElement("script");
+  // useEffect(() => {
+  //   const scriptElement = document.createElement("script");
 
-    scriptElement.type = "module";
-    scriptElement.src =
-      "https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.60/dist/web.js";
+  //   scriptElement.type = "module";
+  //   scriptElement.src =
+  //     "https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.60/dist/web.js";
 
-    document.head.appendChild(scriptElement);
+  //   document.head.appendChild(scriptElement);
 
-    scriptElement.onload = () => {
-      window.Typebot.initBubble({
-        typebot: "mercadao-v1-0v7r6c5",
-        apiHost: "https://bot-ai01.htz01.ligerosmart.ai",
-        theme: {
-          button: { backgroundColor: "#0042DA" },
-          chatWindow: { backgroundColor: "#fff" },
-        },
-      });
-    };
+  //   scriptElement.onload = () => {
+  //     window.Typebot.initBubble({
+  //       typebot: "mercadao-v1-0v7r6c5",
+  //       apiHost: "https://bot-ai01.htz01.ligerosmart.ai",
+  //       theme: {
+  //         button: { backgroundColor: "#0042DA" },
+  //         chatWindow: { backgroundColor: "#fff" },
+  //       },
+  //     });
+  //   };
 
-    return () => {
-      document.head.removeChild(scriptElement);
-    };
-  }, []);
+  //   return () => {
+  //     document.head.removeChild(scriptElement);
+  //   };
+  // }, []);
 
   return null;
 };
